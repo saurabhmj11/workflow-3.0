58db4d038eda7178b22635b98c7b5e163b33e29e
Z User <z@container>
2026-06-19 04:01:56 +0000

    c8cb9b84-9249-4535-9c1e-b0ca76d76d5d

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Start script for OpenWorkflow production server
-# Ignores SIGHUP to persist after terminal closes
 trap '' SIGHUP
 trap '' SIGPIPE
+trap '' SIGTERM
 cd /home/z/my-project
 exec npx next start -p 3000 -H 0.0.0.0
